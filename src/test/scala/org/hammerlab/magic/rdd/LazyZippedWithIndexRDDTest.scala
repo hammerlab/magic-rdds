572ccd073a48f1d9c003555898cc0b57790dffbb
Ryan Williams <ryan.blake.williams@gmail.com>
2016-08-20 21:49:11 +0000

    lzwi tweaks

--- src/test/scala/org/hammerlab/magic/rdd/LazyZippedWithIndexRDDTest.scala
+++ src/test/scala/org/hammerlab/magic/rdd/LazyZippedWithIndexRDDTest.scala
@@ -1,43 +1,31 @@
 package org.hammerlab.magic.rdd
 
 import org.hammerlab.magic.rdd.LazyZippedWithIndexRDD._
-import org.hammerlab.magic.test.listener.TestSparkListener
-import org.hammerlab.magic.util.SparkSuite
-import org.scalatest.BeforeAndAfter
+import org.hammerlab.magic.test.listener.TestListenerSuite
+import org.hammerlab.magic.test.spark.SparkCasesSuite
 
-class LazyZippedWithIndexRDDTest extends SparkSuite with BeforeAndAfter {
-
-  def listener = TestSparkListener()
-  def numStages = listener.stages.size
-
-  override def beforeAll() {
-    super.beforeAll()
-    sc.addSparkListener(listener)
-  }
-
-  before {
-    listener.clear()
-  }
-
-  test("initialize lazy zipWithIndex") {
-    val rdd = sc.parallelize(0 until 10)
-    val lazyIndexed = rdd.lazyZipWithIndex
-    numStages should be (0)
-  }
+class LazyZippedWithIndexRDDTest
+  extends SparkCasesSuite
+    with TestListenerSuite {
 
   test("match with zipWithIndex") {
     val rdd = sc.parallelize(0 until 10).filter(_ % 2 == 0)
 
     val lazyIndexed = rdd.lazyZipWithIndex
+
+    numStages should be(0)
+
     val indexed = rdd.zipWithIndex
 
     // only zipWithIndex will trigger job when initialized
     numStages should be (1)
 
     val res1 = lazyIndexed.collect
+
+    numStages should be(3)
+
     val res2 = indexed.collect
-    // 2 stages for each RDD (computing count for partitions and indexing each element)
-    // note that 1 stage is run for 'zipWithIndex' and 2 stages - for 'lazyZipWithIndex'
+
     numStages should be (4)
 
     res1 should be (res2)
