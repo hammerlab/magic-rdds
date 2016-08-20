02dd133256dc37b0f461cdf65801ca910755c44b
Ryan Williams <ryan.blake.williams@gmail.com>
2016-08-20 20:42:33 +0000

    TestSparkListener fixes, TestListenerSuite mix-in

--- src/test/scala/org/hammerlab/magic/util/SparkSuite.scala
+++ src/test/scala/org/hammerlab/magic/util/SparkSuite.scala
@@ -3,7 +3,11 @@ package org.hammerlab.magic.util
 import com.holdenkarau.spark.testing.SharedSparkContext
 import org.scalatest.{FunSuite, Matchers}
 
-trait SparkSuite extends FunSuite with SharedSparkContext with Matchers {
+trait SparkSuite
+  extends FunSuite
+    with SharedSparkContext
+    with Matchers {
+
   implicit lazy val sparkContext = sc
 
   override def beforeAll(): Unit = {
