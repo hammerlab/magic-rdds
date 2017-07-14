d47efa792031c7590246d96f29e081502996c1a6
Ryan Williams <ryan.blake.williams@gmail.com>
2017-07-14 01:31:32 +0000

    make Timer a mix-in

--- src/main/scala/org/hammerlab/timing/Timer.scala
+++ src/main/scala/org/hammerlab/timing/Timer.scala
@@ -1,13 +1,17 @@
 package org.hammerlab.timing
 
-object Timer {
+import grizzled.slf4j.Logging
+
+trait Timer {
+  self: Logging ⇒
+
   def time[T](fn: => T): T = time("")(fn)
 
   def time[T](msg: ⇒ String)(fn: => T): T = {
     val before = System.currentTimeMillis
     val res = fn
     val after = System.currentTimeMillis
-    println(s"$msg:\t${after-before}ms")
+    info(s"$msg:\t${after-before}ms")
     res
   }
 }
